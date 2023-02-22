0e4a2e84dd3e4258af978fd11db07ac5106822fd
mauroalvarez <mauro.alvarez.jr0@gmail.com>
2023-02-22 01:41:40 -0600

    email validators

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -23,7 +23,7 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), Email(), user_exists])
+    email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired()])
     firstname = StringField('first name', validators=[DataRequired()])
     lasttname = StringField('last name', validators=[DataRequired()])
