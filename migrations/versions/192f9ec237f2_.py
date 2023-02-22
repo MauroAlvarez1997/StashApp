933d3e7d5e618afd7afd380fa316de8f528befa1
mauroalvarez <mauro.alvarez.jr0@gmail.com>
2023-02-22 01:49:27 -0600

    added more to migrations versions

--- migrations/versions/192f9ec237f2_.py
+++ migrations/versions/192f9ec237f2_.py
@@ -13,7 +13,7 @@ environment = os.getenv("FLASK_ENV")
 SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
-revision = '192f9ec237f2'
+revision = 'ffdc0a98111c'
 down_revision = None
 branch_labels = None
 depends_on = None
